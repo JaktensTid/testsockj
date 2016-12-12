3bd003273ee456c7495102fc7162a119caabcaab
JaktensTid <jaktenstid1@gmail.com>
2016-12-12 16:07:57 +0300

    Sock

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var WebSocketServer = new require('ws');
 var clients = {};
 
 // WebSocket-сервер на порту 8081
-var webSocketServer = new WebSocketServer.Server({port: process.env.PORT});
+var webSocketServer = new WebSocketServer.Server({port: 8081});
 webSocketServer.on('connection', function(ws) {
 
   var id = Math.random();
@@ -17,7 +17,7 @@ webSocketServer.on('connection', function(ws) {
     console.log('получено сообщение ' + message);
 
     for(var key in clients) {
-      clients[key].send(message + '|' + process.env.PORT);
+      clients[key].send(message);
     }
   });
 
