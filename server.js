4992d9325c718051c25e0f91a0e34cd6f108d69a
JaktensTid <jaktenstid1@gmail.com>
2016-12-12 16:29:37 +0300

    Node js merged with WebSocket

--- server.js
+++ server.js
@@ -1,41 +1,27 @@
-var http = require('http');
-var Static = require('node-static');
-var WebSocketServer = new require('ws');
+'use strict';
 
-// подключенные клиенты
-var clients = {};
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-// WebSocket-сервер на порту 8081
-var webSocketServer = new WebSocketServer.Server({port: 8081});
-webSocketServer.on('connection', function(ws) {
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-  var id = Math.random();
-  clients[id] = ws;
-  console.log("новое соединение " + id);
+const server = express()
+  .use((req, res) => res.sendFile(INDEX) )
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-  ws.on('message', function(message) {
+const wss = new SocketServer({ server });
+
+wss.on('connection', (ws) => {
+  console.log('Client connected');
+  ws.on('close', () => console.log('Client disconnected'));
+});
+
+wss.on('message', function(message) {
     console.log('получено сообщение ' + message);
 
-    for(var key in clients) {
+    for (var key in wss.clients) {
       clients[key].send(message);
     }
   });
-
-  ws.on('close', function() {
-    console.log('соединение закрыто ' + id);
-    delete clients[id];
-  });
-
-});
-
-
-// обычный сервер (статика) на порту 8080
-var fileServer = new Static.Server('.');
-http.createServer(function (req, res) {
-  
-  fileServer.serve(req, res);
-
-}).listen(process.env.PORT);
-
-console.log("Сервер запущен на порте " + process.env.PORT);
-
